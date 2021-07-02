a98cea13c70c77a6923ae149088e9d9cc542a756
Yonis-Espitia <yezo2905@gmail.com>
2021-07-02 15:11:31 -0500

    correccion homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,20 +5,41 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+  } else if (y > x){
+    return y;
+  } else {
+    return x;
+  }
+
+
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-}
-  
+
+  if(edad >= 18){
+    return "Allowed";
+  } else if(edad < 18) {
+    return "Not allowed";
+  }
+} 
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+
+  if (status === 1){
+    return "Online";
+  } else if (status === 2){
+    return "Away";
+  }
+    
 }
 
 function saludo(idioma) {
@@ -28,7 +49,20 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-}
+
+  // Punto 1
+  if(idioma === "aleman"){
+  return "Guten Tag!";
+  } else  if (idioma === "mandarin"){
+  return "Ni Hao!";
+  } else if (idioma === "ingles"){
+  return "Hello!";
+  } else {
+  return "Hola";
+  }
+  
+} 
+  
 
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
@@ -91,6 +125,11 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
+  if (x === y){
+    return "Soy verdadero"
+  }
+  return "Soy falso";
+
 }
 
 function tablaDelSeis(){
@@ -104,6 +143,12 @@ function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   
+  if (x > 99){
+    return "true";
+  }else {
+    return "false";
+  } 
+    
 }
 
 function doWhile(numero) {
